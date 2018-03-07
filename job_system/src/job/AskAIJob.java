840c5a5032dd83600ea551ff8b490f725d884f68
whizkid <kaustubhd06@gmail.com>
2018-03-07 00:49:38 -0800

    modified AskAI job

--- job_system/src/job/AskAIJob.java
+++ job_system/src/job/AskAIJob.java
@@ -2,11 +2,6 @@ package job;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import oracle.tape.acs.summit.librarycontroller.common.entities.ValidationException;
-import oracle.tape.acs.summit.librarycontroller.common.entities.exception.InternalLibraryException;
-import oracle.tape.acs.summit.librarycontroller.common.entities.job.AbstractJob;
-import oracle.tape.acs.summit.librarycontroller.common.entities.request.RequestOutputParameter;
 import request.Request;
 
 import javax.xml.bind.ValidationException;
@@ -14,20 +9,15 @@ import javax.xml.bind.ValidationException;
 import entities.AI;
 import message.AIResponseMessage;
 
-public class AskAIJob extends AbstractJob {
+public class AskAIJob extends GenericJob {
 	
+	AskAIJob(JobType type, long requestId) {
+		super(type, requestId);
+	}
+
 	private String AI_KEY       = "AI_KEY";
 	private String QUESTION_KEY = "QUESTION_KEY";
 	
-	public AskAIJob(
-			String Question,
-			AI ai,
-			AbstractJob parentJob,
-			long requestId) {
-
-        super(JobType.QUESTION_SINGLE_AI, parentJob, requestId);
-    }
-
 	@Override
 	protected void createInitialResources() {
 		
@@ -40,19 +30,19 @@ public class AskAIJob extends AbstractJob {
 		JobParameter aiParam =
 				new JobParameter(
 						0L,
-						this.getId(),
+						this.id_,
 						ParameterType.AI,
 						"AI_Value");
 		
 		JobParameter questionParam =
 				new JobParameter(
 						0L,
-						this.getId(),
+						this.id_,
 						ParameterType.Question,
 						"Question_Value");
 		
-		this.getJobParameters().put(AI_KEY, aiParam);
-		this.getJobParameters().put(QUESTION_KEY, questionParam);
+		this.parameters_.put(AI_KEY, aiParam);
+		this.parameters_.put(QUESTION_KEY, questionParam);
 	}
 	
 	/**
@@ -76,7 +66,7 @@ public class AskAIJob extends AbstractJob {
      * @return List<Job>
      */
     @Override
-    public List<AbstractJob> startJobBasic() throws Exception {
+    public ExecutionResult startJobBasic() {
 
     	boolean result = false;
     	
@@ -92,21 +82,16 @@ public class AskAIJob extends AbstractJob {
         	result = ai.askQuestion(this.getQuestion());
         }
         
-        return result ? new ArrayList<AbstractJob>() : null;
+        if(result) {
+        	
+        	this.state_ = JobState.WAITING;
+        	this.min_ = WAITING_FOR_AI_RESPONSE_MIN;
+        	this.max_ = WAITING_FOR_AI_RESPONSE_MAX;
+        }
+        
+        return new ExecutionResult(result);
     }
-    
-    /**
-     * Handle the AI Response
-     */
-    @Override
-    public List<AbstractJob> deviceResponseBasic(AIResponseMessage jobDeviceResponse)
-            throws Exception {
-
-    	//Persist the answer 
-    	
-    	return new ArrayList<AbstractJob>();
-    }
-    
+        
     protected void validateAI() throws ValidationException {
     	
     	this.getAI(); 
@@ -114,16 +99,16 @@ public class AskAIJob extends AbstractJob {
 
     protected void validateQuestion() throws ValidationException {
     	
-    	String Question = (String)this.getJobParameters().get(QUESTION_KEY).getValue();
+    	String Question = (String)this.parameters_.get(QUESTION_KEY).getValue();
     }
     
     protected AI getAI() {
     	
-    	return (AI)this.getJobParameters().get(AI_KEY).getValue();
+    	return (AI)this.parameters_.get(AI_KEY).getValue();
     }
     
     protected String getQuestion() {
     	
-    	return (String)this.getJobParameters().get(QUESTION_KEY).getValue();
+    	return (String)this.parameters_.get(QUESTION_KEY).getValue();
     }
 }
