982ccc677111f613b42426128c469adbb2efb068
ultimatetag <david@ultimatetag.com>
2018-05-28 13:08:04 -0700

    adding attributions for the icons we are using

--- ai-ui/src/app/AboutDialog/AboutDialog.tsx
+++ ai-ui/src/app/AboutDialog/AboutDialog.tsx
@@ -16,22 +16,54 @@ export interface IAboutDialogProps {
 export class AboutDialog extends React.Component<IAboutDialogProps, IAboutDialogState> {
   public AboutTextField: TextField;
 
-  constructor(props: IAboutDialogProps) {
-    super(props);
+  public attributions = () => {
+    return (<pre style={{ textAlign: 'left' }}>{`
+      Avatar Icons provided under the Creative Commons 3.0 BY
+      or CC0 if mentioned below:
+      - Lorc, http://lorcblog.blogspot.com
+      - Delapouite, http://delapouite.com
+      - John Colburn, http://ninmunanmu.com
+      - Felbrigg, http://blackdogofdoom.blogspot.co.uk
+      - John Redman, http://www.uniquedicetowers.com
+      - Carl Olsen, https://twitter.com/unstoppableCarl
+      - Sbed, http://opengameart.org/content/95-game-icons
+      - PriorBlue
+      - Willdabeast, http://wjbstories.blogspot.com
+      - Viscious Speed, http://viscious-speed.deviantart.com - CC0
+      - Lord Berandas, http://berandas.deviantart.com
+      - Irongamer, http://ecesisllc.wix.com/home
+      - HeavenlyDog, http://www.gnomosygoblins.blogspot.com
+      - Lucas
+      - Faithtoken, http://fungustoken.deviantart.com
+      - Skoll
+      - Andy Meneely, http://www.se.rit.edu/~andy/
+      - Cathelineau
+      - Kier Heyl
+      - Aussiesim
+      - Sparker, http://citizenparker.com
+      - Zeromancer - CC0
+      - Rihlsul
+      - Quoting
+      - Guard13007, https://guard13007.com
+      - DarkZaitzev, http://darkzaitzev.deviantart.com
+      - SpencerDub
+      - GeneralAce135
+      - Zajkonur
+      More info and icons available at https://game-icons.net
+    `}</pre>);
+  };
 
-    this.dismiss = this.dismiss.bind(this);
-  }
-
-  public async dismiss(/*About?: IAbout*/) {
+  public dismiss = async (/*About?: IAbout*/) => {
     this.props.doneAction(/*About*/);
-  }
+  };
 
   public render(): React.ReactNode {
     const margin = { margin: '5px' };
     return (
       <Dialog headerText={'About'} {...this.props} >
-        TODO
         <div className="ms-textAlignCenter">
+
+          {this.attributions()}
           <PrimaryButton onClick={this.dismiss} style={margin}>Back</PrimaryButton>
         </div>
       </Dialog>
