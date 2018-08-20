92682975f364c2443bf464d0389cd17592c29b08
whizkid <kaustubhd06@gmail.com>
2018-08-20 00:19:37 -0700

    Added logic for Http redirects, refactoring

--- job_system/src/entities/AI.java
+++ job_system/src/entities/AI.java
@@ -2,6 +2,7 @@ package entities;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -13,6 +14,10 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import javax.net.ssl.HttpsURLConnection;
 import org.json.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 
 public class AI {
@@ -40,8 +45,6 @@ public class AI {
 
 		try {
 			
-			String endpoint = "http://www.cleverbot.com/getreply?key=CC8ktzqcHDfaOP5T7OnM432weZA&MXYxCTh2MQlBdkFZQVNONlVUN0oJMUZ2MTUyNzE0MTMwOAk2NGlNw6RkY2hlbi4J";
-			
 			String encodedUrlParam = URLEncoder.encode(question.text, "UTF-8");
 			String urlEndpoint = this.address + "&input=" + encodedUrlParam;
 			
@@ -58,23 +61,28 @@ public class AI {
 		}
 	}
 
-	//No connection or timeout set for async call
+	/**
+	 * 
+	 * @param url
+	 * @return
+	 */
 	public String getJSON(String url) {
 	    HttpURLConnection c = null;
 	    try {
-	        URL u = new URL(url);
-	        c = (HttpURLConnection) u.openConnection();
-	        c.setRequestMethod("GET");
-	        c.setRequestProperty("Content-length", "0");
-			c.setRequestProperty("User-Agent", "Mozilla/5.0");
-			c.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-			
-	        c.setUseCaches(false);
-	        c.setAllowUserInteraction(false);
-	        //c.setConnectTimeout(timeout);
-	        //c.setReadTimeout(timeout);
+
+	    	c = getHttpClient(url);
 	        c.connect();
 	        int status = c.getResponseCode();
+	        
+	        boolean redirect = isRedirected(status);
+	    	
+    		// get redirect url from "location" header field	    	
+	    	if (redirect) {
+
+	    		c = getRedirectedClient(c);
+	    		c.connect();
+	    		status = c.getResponseCode();
+	    	}
 
 	        switch (status) {
 	            case 200:
@@ -89,23 +97,119 @@ public class AI {
 	                return sb.toString();
 	        }
 
-	    } catch (MalformedURLException ex) {
-	        //Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
-	    } catch (IOException ex) {
+	        //TODO - Add logging here
+	    } catch (MalformedURLException ex) { }
+	    catch (IOException ex) {
 			System.out.println(ex.getMessage());
-	        //Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
 	    } finally {
 	       if (c != null) {
 	          try {
 	              c.disconnect();
-	          } catch (Exception ex) {
-	             //Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
-	          }
+	          } catch (Exception ex) { }
 	       }
 	    }
 	    return null;
 	}
 	
+	/**
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public HttpURLConnection getHttpClient(String url) {
+		
+		HttpURLConnection c = null;
+		
+		try {
+			URL u = new URL(url);
+			c = (HttpURLConnection) u.openConnection();
+			c.setRequestMethod("GET");
+			c.setRequestProperty("Content-length", "0");
+			c.setRequestProperty("User-Agent", "Mozilla/5.0");
+			c.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+		
+			c.setUseCaches(false);
+			c.setAllowUserInteraction(false);
+			//c.setConnectTimeout(timeout);	//TODO - No connection or timeout set for async call
+			//c.setReadTimeout(timeout);
+        
+			return c;
+		}
+		catch (MalformedURLException ex) { }
+		catch (IOException ex) { }
+		finally {
+			if (c != null) {
+				try {
+					c.disconnect();
+				} catch (Exception ex) { }
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param c
+	 * @return
+	 */
+	public HttpURLConnection getRedirectedClient(HttpURLConnection c) {
+		
+		// get redirect url from "location" header field
+		String newUrl = c.getHeaderField("Location");
+
+		// get the cookie if need, for login
+		String cookies = c.getHeaderField("Set-Cookie");
+
+		try {
+			// open the new connnection again
+			c = (HttpURLConnection) new URL(newUrl).openConnection();
+			c.setRequestProperty("Cookie", cookies);
+			c.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
+			c.addRequestProperty("User-Agent", "Mozilla");
+			c.addRequestProperty("Referer", "google.com");
+		
+			return c;
+		}
+		 catch (MalformedURLException ex) {
+		        //Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+		 } catch (IOException ex) {
+				System.out.println(ex.getMessage());
+		        //Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+		 } finally {
+			 if (c != null) {
+				 try {
+					 c.disconnect();
+				 } catch (Exception ex) {
+		             //Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+				 }
+			 }
+		 }
+		
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param status
+	 * @return
+	 */
+	public boolean isRedirected(int status) {
+		
+		boolean redirect = false;
+		if (status != HttpURLConnection.HTTP_OK) {
+    		if (status == HttpURLConnection.HTTP_MOVED_TEMP
+    			|| status == HttpURLConnection.HTTP_MOVED_PERM
+    				|| status == HttpURLConnection.HTTP_SEE_OTHER)
+    		redirect = true;
+    	}
+		
+		return redirect;
+	}
+	
+	/**
+	 * 
+	 */
 	public void GoogleCallCode() {
 		
 		//URL url = new URL("http://www.google.com/search?q=mkyong");
@@ -122,46 +226,76 @@ public class AI {
 	    //return the answer
 	}
 	
+	/**
+	 * 
+	 */
 	public void sendPost() {
 
 		try {
-		String url = "https://selfsolve.apple.com/wcResults.do";
-		URL obj = new URL(url);
-		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+			String url = "https://selfsolve.apple.com/wcResults.do";
+			URL obj = new URL(url);
+			HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
 
-		//add reuqest header
-		con.setRequestMethod("POST");
-		con.setRequestProperty("User-Agent", "Mozilla/5.0");
-		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+			//add reuqest header
+			con.setRequestMethod("POST");
+			con.setRequestProperty("User-Agent", "Mozilla/5.0");
+			con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
 
-		String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+			String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
 		
-		// Send post request
-		con.setDoOutput(true);
-		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-		wr.writeBytes(urlParameters);
-		wr.flush();
-		wr.close();
+			// Send post request
+			con.setDoOutput(true);
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(urlParameters);
+			wr.flush();
+			wr.close();
 
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Post parameters : " + urlParameters);
-		System.out.println("Response Code : " + responseCode);
+			int responseCode = con.getResponseCode();
+			System.out.println("\nSending 'POST' request to URL : " + url);
+			System.out.println("Post parameters : " + urlParameters);
+			System.out.println("Response Code : " + responseCode);
 
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
 
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
 		
-		//print result
-		System.out.println(response.toString());
+			//print result
+			System.out.println(response.toString());
 		}
 		catch(Exception e) { }
+	}
+	
+	/**
+	 * This is future work for webscraping
+	 */
+	static void parsePostCallFromSite() {
+		
+		try {
+			Document doc1 = Jsoup.connect("http://example.com/").get();
+			String title = doc1.title();
+			File input = new File("C:\\Users\\whizkid\\Documents\\changedFile.html");
+			Document doc = Jsoup.connect("http://www.square-bear.co.uk/mitsuku/nfchat.htm").get();
+		
+			Document doc2 = Jsoup.parse(input, "UTF-8", "http://www.square-bear.co.uk/mitsuku/nfchat.htm");
+		
+			String res = doc2.toString();
+		
+			Element content = doc.getElementById("content");
+			Elements links = content.getElementsByTag("a");
+			for (Element link : links) {
+				String linkHref = link.attr("href");
+				String linkText = link.text();
+			}
+		}
+		catch(Exception e) {
 
+			System.out.println(e.getMessage());
+		}
 	}
 }
