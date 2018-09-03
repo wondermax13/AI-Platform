2dd0e63a68d93b491d2bc5ffb1bf6b1ea61a8dab
ultimatetag <david@ultimatetag.com>
2018-09-03 02:33:15 -0700

    fixing the question feed

--- ai-ui/src/app/components/Feed/Feed.tsx
+++ ai-ui/src/app/components/Feed/Feed.tsx
@@ -1,9 +1,34 @@
-// import { app as appCss, section as sectionCss, column as columnCss } from './App.scss';
-import { DocumentCard, DocumentCardActions, DocumentCardActivity, DocumentCardLocation, DocumentCardTitle, DocumentCardType } from 'office-ui-fabric-react';
+import {
+  Persona,
+  PersonaSize
+} from 'office-ui-fabric-react';
 import * as React from 'react';
 import { ICommon } from '../../models/Common';
 import { IAnswer, IQuestionModel } from '../../models/Question';
 import { avatarByCyclingIndex } from '../../providers/avatars';
+
+const personaGrid: React.CSSProperties = {
+  display: 'grid',
+  gridTemplateAreas: `
+    'picon pname'
+    'picon ptext'
+  `,
+  gridTemplateColumns: '40px auto',
+  gridTemplateRows: 'min-content min-content',
+  marginTop: 10,
+  marginBottom: 10,
+};
+
+// const rightPersonaGrid: React.CSSProperties = {
+//   ...personaGrid,
+//   gridTemplateAreas: `
+//     'pname . picon'
+//     'ptext . picon'
+//   `,
+//   gridTemplateColumns: 'auto 10px 40px',
+//   gridTemplateRows: 'min-content min-content',
+//   textAlign: 'right',
+// }
 
 export class Feed extends React.Component<ICommon, {}> {
   constructor(props: ICommon) {
@@ -16,7 +41,6 @@ export class Feed extends React.Component<ICommon, {}> {
   }
 
   public getLocation(question: IQuestionModel): string {
-    // return question.to.reduce((acc: string, nex: { type: string, value: string }) => acc + (acc.length > 0 ? ', ' : '') + nex.value, '');
     return question.channels.reduce((acc: string, nex: string) => acc + (acc.length > 0 ? ', ' : '') + nex, '');
   }
 
@@ -25,60 +49,68 @@ export class Feed extends React.Component<ICommon, {}> {
   }
 
   public renderAnswer(answer: IAnswer, index: number) {
-    return <DocumentCardActivity
-      activity={answer.answer}
-      people={
-        [
-          {
-            name: `${answer.ai && answer.ai.name || answer.aiId}`,
-            profileImageSrc: answer.ai && answer.ai.avatar && `client/avatars/${answer.ai.avatar}` || avatarByCyclingIndex(index)
-          }
-        ]
-      }
-    />
+    return (
+      <div style={personaGrid}>
+        <Persona
+          styles={{ root: { gridArea: 'picon' } }}
+          size={PersonaSize.size32}
+          // imageUrl='https://static2.sharepointonline.com/files/fabric/office-ui-fabric-react-assets/persona-female.png'
+          imageUrl={answer.ai && answer.ai.avatar && `client/avatars/${answer.ai.avatar}` || avatarByCyclingIndex(index)}
+          imageInitials='A3'
+        />
+        <text style={{ gridArea: 'pname' }} className='ms-font-xs  ms-fontWeight-bold'>{answer.ai && answer.ai.name || answer.aiId}</text>
+        <text style={{ gridArea: 'ptext' }} className='ms-font-s'>
+          {answer.answer}
+        </text>
+      </div>
+    )
   }
 
   public renderQuestion(item: IQuestionModel, index: number) {
-    return <DocumentCard
-      key={index}
-      type={DocumentCardType.normal}
-    >
-      <div className="ms-DocumentCard-details">
-        <DocumentCardLocation
-          location={this.getLocation(item)}
-          ariaLabel={this.getLocation(item)}
-          onClick={this.onClickLocation}
 
-        />
-        <DocumentCardTitle
-          title={item.question}
-          shouldTruncate={false}
-        />
-        <DocumentCardActions
-          actions={
-            [
-              {
-                ariaLabel: 'share action',
-                iconProps: { iconName: 'Share' },
-              },
-              {
-                ariaLabel: 'pin action',
-                iconProps: { iconName: 'Pin' },
-              },
-              {
-                ariaLabel: 'ringer action',
-                iconProps: { iconName: 'Ringer' },
-              },
-            ]
-          }
-          views={432}
-        />
-        {item.answers!.slice(0, 10).map(this.renderAnswer)}
+    // const questioner: IPersonaSharedProps = {
+    //   imageUrl: 'https://static2.sharepointonline.com/files/fabric/office-ui-fabric-react-assets/persona-female.png',
+    //   imageInitials: 'AL',
+    //   text: 'Annie Lindqvist',
+    //   secondaryText: 'Software Engineer',
+    //   tertiaryText: 'In a meeting',
+    //   optionalText: 'Available at 4:00pm'
+    // };
+
+    const answerItems = item.answers && item.answers.slice(0, 10) || [];
+    const answers = answerItems.map(this.renderAnswer);
+
+    return (
+      <div key={index} style={{ borderBottom: '1pt solid #eee', padding: 10 }}>
+        <div style={personaGrid}>
+          <Persona
+            size={PersonaSize.size32}
+            styles={{ root: { gridArea: 'picon' } }}
+            // imageUrl='https://static2.sharepointonline.com/files/fabric/office-ui-fabric-react-assets/persona-female.png'
+            imageInitials='A3'
+          />
+          <div style={{ gridArea: 'pname' }} className='ms-font-s ms-fontWeight-bold'>
+            <text className='ms-font-xs' style={{ float: 'right' }}>
+              {this.getLocation(item)}
+            </text>
+            {'Annie Lindqvist'}
+          </div>
+          <text style={{ gridArea: 'ptext' }} className='ms-font-m-plus'>
+            {item.question}
+          </text>
+        </div>
+        <div style={{ margin: 20 }}>
+          {answers}
+        </div>
       </div>
-    </DocumentCard>
+    );
   }
 
   public render(): React.ReactNode {
-    return this.props.questions.slice(0, 15).map(this.renderQuestion);
+    return (
+      <div style={{ boxShadow: '#333 1px -1px 67px' }}>
+        {this.props.questions.slice(0, 15).map(this.renderQuestion)}
+      </div>
+    )
   }
 }
